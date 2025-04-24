ac907b2a330eff25560848fb73293a6c4aeab332
Liam V <liamvickerstaff050@gmail.com>
2025-04-24 18:26:10 +1000

    Project Initialized + Some Tests

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1>BoulderSoc</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Buttons/JoinTodayBtn.tsx
@@ -0,0 +1,10 @@
+export default function JoinTodayBtn() {
+  return (
+    <a
+      className="btn join-today btn-typography"
+      href="https://campus.hellorubric.com/?tab=memberships&s=6444&fbclid=PAZXh0bgNhZW0CMTEAAacs1V8o-DrcKrzM3vo7f1gNaKl_doI4vQ6s41aPGkE1hi-vC9AuR01HQNdFiw_aem_ShqbibJ4H3PSXwb1tbfZsg"
+    >
+      Join Today
+    </a>
+  );
+}
